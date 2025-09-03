52dc0c7a50827f58c20d88fd4d53bf113a353f42
Tolga Taner <dev.tolgataner@icloud.com>
2025-09-03 13:18:30 +0300

    add modifier syntax

--- Sources/NavFlow/NavFlowViewModifiers.swift
+++ Sources/NavFlow/NavFlowViewModifiers.swift
@@ -13,10 +13,48 @@ public extension NavFlowNavigationBarView {
         self.toolbar(.hidden, for: .navigationBar)
     }
 }
+
+public extension View {
+    func makeNavigationBar(backgroundColor: Color, height: NavigationBarHeight) -> some View {
+        ZStack {
+            Color.clear
+                .ignoresSafeArea()
+        NavigationStack {
+            VStack(spacing: 0) {
+                     self
+                    .foregroundStyle(.white)
+                    .frame(height: height.value)
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        backgroundColor.ignoresSafeArea(edges: .top)
+                    )
+            }
+            .navigationBarBackButtonHidden(true)
+        }
+        }
+    }
+
+    func makeNavigationBar(path: Binding<NavigationPath>, backgroundColor: Color, height: NavigationBarHeight) -> some View {
+        ZStack {
+            Color.clear
+                .ignoresSafeArea()
+        NavigationStack(path: path) {
+            VStack(spacing: 0) {
+                     self
+                    .foregroundStyle(.white)
+                    .frame(height: height.value)
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        backgroundColor.ignoresSafeArea(edges: .top)
+                    )
+            }
+            .navigationBarBackButtonHidden(true)
+        }
+        }
+    }
+}
 #else
 public extension NavFlowNavigationBarView {
     func hideNavFlowNavigationBar() -> some View { self }
 }
 #endif
-
-
